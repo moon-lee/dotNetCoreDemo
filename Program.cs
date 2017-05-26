9b1feab652e990989e90f84892ae1e94a8d2d3b6
Moon Lee <m.lee@siama.com.au>
2017-05-26 13:45:56 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotNetCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Moon Lee";
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
