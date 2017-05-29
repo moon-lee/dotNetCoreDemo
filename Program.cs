eca54c7d0db1b64e65337204a48699aedc0b8858
Moon Lee <m.lee@siama.com.au>
2017-05-29 20:17:19 +1000

    add read file

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.IO;
 
-namespace dotNetCoreDemo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var name = "Moon Lee";
-            Console.WriteLine($"Hello {name}!");
+namespace dotNetCoreDemo {
+    class Program {
+
+        public static string datafile = "data.txt";
+        static void Main (string[] args) {
+            using (var fs = new FileStream (datafile, FileMode.Open, FileAccess.ReadWrite)) {
+                using (var sr = new StreamReader (fs)) {
+                    string line;
+                    while ((line = sr.ReadLine ()) != null) {
+                        // process the line
+
+                        readNode(line);
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+          
+        }
+
+        public static bool readNode(String node) {
+            return true;
         }
     }
 }
